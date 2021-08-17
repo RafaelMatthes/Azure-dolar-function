39b85ba5075f33a4016f29374a5003518956f7cc
rafael <rafael.matthes@live.com>
2021-08-17 16:38:53 -0300

    feat: add class and function to get values

--- HttpTrigger-dolar/__init__.py
+++ HttpTrigger-dolar/__init__.py
@@ -1,24 +1,34 @@
 import logging
-
+import json
 import azure.functions as func
+from .classes import Dolar
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    try:
+        dolar = Dolar()
+    
+        value = dolar.get_value()#req.params.get('name')
+    except:
+        value = 'teste'
+
+
+    if not value:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            value = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if value:
+        return func.HttpResponse(
+            json.dumps(value)
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            {'error':'500'},
+            status_code=200
         )
---
+++ HttpTrigger-dolar/classes.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import xmltodict
+from datetime import date
+
+class Dolar(object):
+
+    def __init__(self):
+        self._url = 'https://www3.bcb.gov.br/bc_moeda/rest/converter/1/1/220/790/'
+        self.data_atual = date.today()        
+
+    def get_value(self):
+        header = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+        page = requests.get(f'{self._url}{self.data_atual.year}-{self.data_atual.month}-{self.data_atual.day}', headers=header)
+        
+        try:
+            if page.status_code == 200:
+                valor = json.loads(json.dumps(xmltodict.parse(page.content)))
+
+                context = {}
+                context['dolar'] = "%.4f" %float(valor['valor-convertido'])
+
+                return context
+        except:
+            return 'erro'
+                
+
+        return {
+            'Dolar':'None', 
+        }
+
+   
