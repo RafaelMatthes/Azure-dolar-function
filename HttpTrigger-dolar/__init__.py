6d23865fd9e791caf840a24ba9fb38184b4dc1bc
rafael <rafael.matthes@live.com>
2021-08-24 21:03:58 -0300

    fix

--- HttpTrigger-dolar/__init__.py
+++ HttpTrigger-dolar/__init__.py
@@ -3,32 +3,22 @@ import json
 import azure.functions as func
 from .classes import Dolar
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
         dolar = Dolar()
     
-        value = dolar.get_value()#req.params.get('name')
+        value = dolar.get_value()
     except:
-        value = 'teste'
-
-
-    if not value:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            value = req_body.get('name')
-
+        value = 'None'
+            
     if value:
         return func.HttpResponse(
             json.dumps(value)
         )
     else:
         return func.HttpResponse(
-            {'error':'500'},
-            status_code=200
+            {'error': '400'},
+            status_code=100
         )
